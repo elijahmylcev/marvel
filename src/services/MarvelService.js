72d637fe58bdf88925676c5dc731e47d59b3ef7d
Elijah Mylcev <elijahmylcev@gmail.com>
2022-04-03 12:15:55 +0300

    Install Eslint

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,14 +1,15 @@
 
-
 class MarvelService {
-  getResource = async (url) => {
-    let res = await fetch(url);
+  async getResource(url) {
+    const res = await fetch(url);
 
     if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
-    return await res.json()
+    const result = await res.json();
+
+    return result;
   }
 }
 
