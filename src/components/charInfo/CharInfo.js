40fadef1c8044e48b3b43cd27f6ede9dbaa67d7d
Elijah Mylcev <elijahmylcev@gmail.com>
2022-04-05 16:50:02 +0300

    fix CharInfo component

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -88,7 +88,12 @@ function View({ char }) {
   return (
     <>
       <div className="char__basics">
-        <img src={thumbnail} alt="abyss" />
+        <img
+          src={thumbnail}
+          alt="abyss"
+          style={thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+            ? { objectFit: 'contain' } : null}
+        />
         <div>
           <div className="char__info-name">{name}</div>
           <div className="char__btns">
@@ -106,13 +111,19 @@ function View({ char }) {
       </div>
       <div className="char__comics">Comics:</div>
       <ul className="char__comics-list">
+        {comics.length > 0 ? null : 'There is no description for this character'}
         {
-        comics.map((item, i) => (
-          <li key={i + 1} className="char__comics-item">
-            {item.name}
-          </li>
-        ))
-      }
+        comics.map((item, i) => {
+          if (i > 9) {
+            return null;
+          }
+          return (
+            <li key={i + 1} className="char__comics-item">
+              {item.name}
+            </li>
+          );
+        })
+        }
 
       </ul>
     </>
