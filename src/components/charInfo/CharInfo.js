1ac11fdc0ccfc6bf76f5de667d1c23a635195b83
Elijah Mylcev <elijahmylcev@gmail.com>
2022-04-05 16:41:15 +0300

    bind CharInfo && CharList

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -2,7 +2,6 @@ import { Component } from 'react';
 import propTypes from 'prop-types';
 import MarvelService from '../../services/MarvelService';
 import './charInfo.scss';
-import thor from '../../resources/img/thor.jpeg';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/Spinner';
 import Skeleton from '../skeleton/Skeleton';
@@ -25,6 +24,13 @@ class CharInfo extends Component {
 
   componentDidMount() {
     this.updateChar();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { charId } = this.props;
+    if (charId !== prevProps.charId) {
+      this.updateChar();
+    }
   }
 
   onCharLoaded(char) {
@@ -76,57 +82,38 @@ class CharInfo extends Component {
 }
 
 function View({ char }) {
+  const {
+    name, description, thumbnail, homepage, wiki, comics,
+  } = char;
   return (
     <>
       <div className="char__basics">
-        <img src={thor} alt="abyss" />
+        <img src={thumbnail} alt="abyss" />
         <div>
-          <div className="char__info-name">thor</div>
+          <div className="char__info-name">{name}</div>
           <div className="char__btns">
-            <a href="/" className="button button__main">
+            <a href={homepage} className="button button__main">
               <div className="inner">homepage</div>
             </a>
-            <a href="/" className="button button__secondary">
+            <a href={wiki} className="button button__secondary">
               <div className="inner">Wiki</div>
             </a>
           </div>
         </div>
       </div>
       <div className="char__descr">
-        descr
+        {description}
       </div>
       <div className="char__comics">Comics:</div>
       <ul className="char__comics-list">
-        <li className="char__comics-item">
-          All-Winners Squad: Band of Heroes (2011) #/3
-        </li>
-        <li className="char__comics-item">
-          Alpha Flight (1983) #/50
-        </li>
-        <li className="char__comics-item">
-          Amazing Spider-Man (1999) #/503
-        </li>
-        <li className="char__comics-item">
-          Amazing Spider-Man (1999) #/504
-        </li>
-        <li className="char__comics-item">
-          AMAZING SPIDER-MAN VOL. 7: BOOK OF EZEKIEL TPB (Trade Paperback)
-        </li>
-        <li className="char__comics-item">
-          Amazing-Spider-Man: Worldwide Vol. 8 (Trade Paperback)
-        </li>
-        <li className="char__comics-item">
-          Asgardians Of The Galaxy Vol. 2: War Of The Realms (Trade Paperback)
-        </li>
-        <li className="char__comics-item">
-          Vengeance (2011) #/4
-        </li>
-        <li className="char__comics-item">
-          Avengers (1963) #/1
-        </li>
-        <li className="char__comics-item">
-          Avengers (1996) #/1
-        </li>
+        {
+        comics.map((item, i) => (
+          <li key={i + 1} className="char__comics-item">
+            {item.name}
+          </li>
+        ))
+      }
+
       </ul>
     </>
   );
